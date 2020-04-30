f701e965752af55a9703af68fc8be1397f6ee064
gywgithub <qingyi_w@outlook.com>
2020-04-30 11:31:31 +0800

    build(docs): update docs

---
+++ docs/precache-manifest.f225dc0938cef5b2a33735b9a09060f1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b55691e3ad69de59a39e",
+    "url": "/vue-vuetify-admin/js/about.ff103ffd.js"
+  },
+  {
+    "revision": "b7994464e9dc04366fe2",
+    "url": "/vue-vuetify-admin/css/app.2dbbc3e4.css"
+  },
+  {
+    "revision": "b7994464e9dc04366fe2",
+    "url": "/vue-vuetify-admin/js/app.b21ebd5a.js"
+  },
+  {
+    "revision": "ded74fd47bf1fa6c053a",
+    "url": "/vue-vuetify-admin/css/chunk-4e68d531.2cd423c4.css"
+  },
+  {
+    "revision": "ded74fd47bf1fa6c053a",
+    "url": "/vue-vuetify-admin/js/chunk-4e68d531.408dfc54.js"
+  },
+  {
+    "revision": "52e3550380ded6bd2854",
+    "url": "/vue-vuetify-admin/css/chunk-5e1841c0.3ea8a59a.css"
+  },
+  {
+    "revision": "52e3550380ded6bd2854",
+    "url": "/vue-vuetify-admin/js/chunk-5e1841c0.9b99a9ff.js"
+  },
+  {
+    "revision": "55d4d3bab431c89b4f4c",
+    "url": "/vue-vuetify-admin/css/chunk-6a355e80.233af834.css"
+  },
+  {
+    "revision": "55d4d3bab431c89b4f4c",
+    "url": "/vue-vuetify-admin/js/chunk-6a355e80.755fb4ae.js"
+  },
+  {
+    "revision": "4e9f7c9336e3fb4e9d96",
+    "url": "/vue-vuetify-admin/css/chunk-f9c7f058.da929144.css"
+  },
+  {
+    "revision": "4e9f7c9336e3fb4e9d96",
+    "url": "/vue-vuetify-admin/js/chunk-f9c7f058.9a327254.js"
+  },
+  {
+    "revision": "75fb3209aec8e057d551",
+    "url": "/vue-vuetify-admin/css/chunk-vendors.3ff55cd7.css"
+  },
+  {
+    "revision": "75fb3209aec8e057d551",
+    "url": "/vue-vuetify-admin/js/chunk-vendors.5d2e86b3.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-vuetify-admin/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "580163273c29dd16297e2c5ddae7f982",
+    "url": "/vue-vuetify-admin/img/avatar.58016327.png"
+  },
+  {
+    "revision": "668cf77251fcacf2d8eaa6dc1668cec1",
+    "url": "/vue-vuetify-admin/img/v-alt.668cf772.svg"
+  },
+  {
+    "revision": "a1aed959befada29b7ac822a55149747",
+    "url": "/vue-vuetify-admin/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-vuetify-admin/robots.txt"
+  },
+  {
+    "revision": "580163273c29dd16297e2c5ddae7f982",
+    "url": "/vue-vuetify-admin/img/avatar.png"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-vuetify-admin/precache-manifest.0bdce447b7bfa7953857f2bc58c1e41a.js"
+  "/vue-vuetify-admin/precache-manifest.f225dc0938cef5b2a33735b9a09060f1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-vuetify-admin"});
