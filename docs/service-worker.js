10a9f6c5dd2f4e0ca2cb5d12a56b9bba977cfbf5
gywgithub <qingyi_w@outlook.com>
2021-06-16 11:18:03 +0800

    build(docs): update docs folder

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vue-vuetify-admin/precache-manifest.96163d6790a1ec025cff5c5382f0b383.js"
+  "/vue-vuetify-admin/precache-manifest.61dc5c4ed78ec2e64aaba5afe87f95b1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-vuetify-admin"});
